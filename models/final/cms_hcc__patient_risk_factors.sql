9c4837d1cc1cb50815272d709a56c568c842a91d
sarah-tuva <sarah@tuvahealth.com>
2023-05-04 11:47:00 -0600

    Fix concat function for cross-database compatibility

--- models/final/cms_hcc__patient_risk_factors.sql
+++ models/final/cms_hcc__patient_risk_factors.sql
@@ -2,27 +2,27 @@ with demographic_factors as (
 
     select
           patient_id
-        , concat(
-              concat(enrollment_status, '-Enrollee')
-            , ' / '
-            , gender
-            , ' / '
-            , age_group
-            , ' / '
-            , case
+        /* concatenate demographic risk factors */
+        , enrollment_status
+            || ' / '
+            || gender
+            || ' / '
+            || age_group
+            || ' / '
+            || case
                 when medicaid_status = 'Yes' then 'Medicaid'
                 else 'Non-Medicaid'
                 end
-            , ' / '
-            , dual_status
-            , ' / '
-            , orec
-            , ' / '
-            , case
+            || ' / '
+            || dual_status
+            || ' / '
+            || orec
+            || ' / '
+            || case
                 when institutional_status = 'Yes' then 'Institutional'
                 else 'Non-Institutional'
                 end
-          ) as description
+          as description
         , coefficient
         , model_version
         , payment_year
@@ -45,7 +45,7 @@ with demographic_factors as (
 
     select
           patient_id
-        , concat(hcc_description,' (HCC ',hcc_code,')') as description
+        , hcc_description || ' (HCC ' || hcc_code || ')' as description
         , coefficient
         , model_version
         , payment_year
